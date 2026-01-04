7801896935c95efb560f7ef144055fb1e01bb922
Charanlokesh22 <charanlokesh2918@gmail.com>
2026-01-04 20:16:38 +0530

    Create mongo.py

---
+++ mongo.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+from app.core.config import MONGO_URL, DATABASE_NAME
+
+client = MongoClient(MONGO_URL)
+db = client[DATABASE_NAME]
+expenses_collection = db["expenses"]
