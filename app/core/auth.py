8cb7e0537de3cb941546314a1b30432ebd76664d
Charanlokesh22 <charanlokesh2918@gmail.com>
2026-01-04 20:17:33 +0530

    Create auth.py

---
+++ app/core/auth.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+from app.core.config import SECRET_KEY, ALGORITHM
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    to_encode["exp"] = datetime.utcnow() + timedelta(hours=2)
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
